fea3d62f75710a029f89a043b2ff53a553f9c4d6
regeciovad <regeciovad@gmail.com>
2015-06-25 11:06:32 +0200

    About page and other

--- alan_project/alan/models.py
+++ alan_project/alan/models.py
@@ -1,3 +1,13 @@
+# Alan project
+# Models.py
+# Version 0.1
+# Author: Dominika Regeciova, xregec00@stud.fit.vutbr.cz
+
 from django.db import models
 
-# Create your models here.
+class Investor(models.Model):
+    name = models.CharField(max_length=42, verbose_name='Name')
+    url = models.URLField(blank=True, verbose_name='Url')
+    
+    def __str__(self):
+        return self.name
--- alan_project/alan/views.py
+++ alan_project/alan/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django_tables2 import RequestConfig
+from django.db.models import Q
+from django.db.models.query import QuerySet
+from .models import *
 
-# Create your views here.
+def about(request):
+    response = "Alan says hey there world!"
+    return render(request, 'alan/about.html', {})
+
+def index(request):
+    response = "Alan says hey there world!"
+    return render(request, 'alan/index.html', {'response':response})
